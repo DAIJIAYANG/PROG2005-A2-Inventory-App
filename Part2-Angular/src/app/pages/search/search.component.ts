cf227dd2e6a9a11c7f19844042a7258e59ef9ab6
DAIJIAYANG <3272103794@qq.com>
2026-03-28 22:30:33 +1000

    Write the Search logic

--- Part2-Angular/src/app/pages/search/search.component.ts
+++ Part2-Angular/src/app/pages/search/search.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { InventoryService, Item } from '../../services/inventory.service';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
-  styleUrl: './search.component.css'
+  styleUrls: ['./search.component.css']
 })
-export class SearchComponent {
+export class SearchComponent implements OnInit {
+  searchTerm: string = '';
+  searchResults: Item[] = [];
+  isSearched: boolean = false;
 
+  constructor(private inventoryService: InventoryService) {}
+
+  ngOnInit(): void {
+    // 初始状态下显示所有商品
+    this.searchResults = this.inventoryService.getAllItems();
+  }
+
+  // 执行搜索
+  onSearch(): void {
+    this.isSearched = true;
+    this.searchResults = this.inventoryService.searchItemsByName(this.searchTerm);
+  }
+
+  // 重置搜索
+  onReset(): void {
+    this.searchTerm = '';
+    this.isSearched = false;
+    this.searchResults = this.inventoryService.getAllItems();
+  }
+
+  // 过滤：仅显示热门商品
+  showPopularOnly(): void {
+    this.isSearched = true;
+    this.searchResults = this.inventoryService.getPopularItems();
+  }
 }
